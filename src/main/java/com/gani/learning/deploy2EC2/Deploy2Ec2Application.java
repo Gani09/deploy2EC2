d7858aa5913c98dec397f797d0aff17c6da9c4b2
moturiganesh <moturiganesh@gmail.com>
2025-03-06 10:03:19 -0500

    initial Commit

---
+++ src/main/java/com/gani/learning/deploy2EC2/Deploy2Ec2Application.java
@@ -0,0 +1,13 @@
+package com.gani.learning.deploy2EC2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Deploy2Ec2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Deploy2Ec2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/gani/learning/deploy2EC2/Deploy2Ec2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gani.learning.deploy2EC2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Deploy2Ec2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
